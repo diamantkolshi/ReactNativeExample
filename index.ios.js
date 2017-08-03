bf5b9cabd79bf7ca280f54b70e5bb13d0d03e4d1
diamantk <diamantkolshi@gmail.com>
2017-08-03 12:46:44 +0200

    import libraries and create app to show text conmponent

---
+++ index.ios.js
@@ -0,0 +1,14 @@
+// Index.android.js place code in here for IOS
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+
+// create a component
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    );
+};
+// Render it to the devise
